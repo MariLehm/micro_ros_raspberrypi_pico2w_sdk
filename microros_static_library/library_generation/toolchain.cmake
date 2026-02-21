5a74b5f2d79f17598eff6f1a003cbb5ff0119741
MariLehm <89742576+MariLehm@users.noreply.github.com>
2026-02-21 16:24:02 +0100

    Change -mfloat-abi to softfp and -mfpu=fpv5-sp-d16 for compatibility reasons with pico sdk 2.2.0

--- microros_static_library/library_generation/toolchain.cmake
+++ microros_static_library/library_generation/toolchain.cmake
@@ -9,7 +9,7 @@ find_program(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 SET(CMAKE_C_COMPILER_WORKS 1 CACHE INTERNAL "")
 SET(CMAKE_CXX_COMPILER_WORKS 1 CACHE INTERNAL "")
 
-set(FLAGS "-O2 -march=armv8-m.main+fp+dsp -mcpu=cortex-m33 -mthumb -mfloat-abi=hard -ffunction-sections -fdata-sections -fno-exceptions -nostdlib -D'RCUTILS_LOG_MIN_SEVERITY=RCUTILS_LOG_MIN_SEVERITY_NONE'" CACHE STRING "" FORCE)
+set(FLAGS "-O2 -march=armv8-m.main+fp+dsp -mcpu=cortex-m33 -mthumb -mfloat-abi=softfp -mfpu=fpv5-sp-d16 -ffunction-sections -fdata-sections -fno-exceptions -nostdlib -D'RCUTILS_LOG_MIN_SEVERITY=RCUTILS_LOG_MIN_SEVERITY_NONE'" CACHE STRING "" FORCE)
 
 set(CMAKE_C_FLAGS_INIT "-std=c11 ${FLAGS} -DCLOCK_MONOTONIC=0 -D'__attribute__(x)='" CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS_INIT "-std=c++17 ${FLAGS} -fno-rtti -DCLOCK_MONOTONIC=0 -D'__attribute__(x)='" CACHE STRING "" FORCE)
